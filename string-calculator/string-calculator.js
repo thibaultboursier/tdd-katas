998a9f5a6a02382db6491aed05b2487b436d4fb0
thibaultboursier <boursier.thibault@gmail.com>
2018-03-05 11:22:36 +0100

    add tests and code for string calculator

---
+++ string-calculator/string-calculator.js
@@ -0,0 +1,25 @@
+function add(numbers) {
+    let negativeNumbers = [];
+
+    if (numbers === '') {
+        return 0;
+    }
+
+    let list = numbers.split(/[\n,]+/);
+    let result = list.reduce((acc, curr) => {
+        if (curr < 0) return negativeNumbers.push(curr);
+        acc += Number(curr);
+        return acc;
+    }, 0);
+
+    if (negativeNumbers.length) {
+        let message = `negatives not allowed : ${negativeNumbers.join(', ')}`;
+        throw new Error(message);
+    }
+    
+    return result;
+}
+
+module.exports = {
+    add
+};
---
+++ string-calculator/string-calculator.test.js
@@ -0,0 +1,28 @@
+const stringCalculator = require('./string-calculator');
+
+describe('String calculator', () => {
+    
+    it('should return zero on empty string', () => {
+        expect(stringCalculator.add('')).toBe(0);
+    })
+
+    it('should return single number', () => {
+        expect(stringCalculator.add('1')).toBe(1);
+        expect(stringCalculator.add('18')).toBe(18);        
+    })
+
+    it('should calculate multiple numbers', () => {
+        expect(stringCalculator.add('1,2')).toBe(3);
+        expect(stringCalculator.add('1,2,8,10,40')).toBe(61);
+        expect(stringCalculator.add('6\n9')).toBe(15);
+    })
+
+    it('should throw an error when some number is negative', () => {
+        expect(() => {
+            stringCalculator.add('-8');
+        }).toThrow();
+        expect(() => {
+            stringCalculator.add('-2,7,-9');
+        }).toThrow();
+    })
+});
